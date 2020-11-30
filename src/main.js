bf19e648a93f21ea3df2be35775b6185ad572c91
Imesh Gunasekara <imesh.g@blackvt.com>
2020-11-30 22:25:33 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
+import createApp from 'vue'
+import router from './router.js'
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = new createApp(App);
+
+app.use(router);
+
+app.mount('#app');
